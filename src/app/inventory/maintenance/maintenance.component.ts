628ec9f306f0cc7fcbb0af3cf46a3faee9aa4830
Syed <ssinezdev07@gmail.com>
2018-04-06 17:49:12 +0300

    invenroty popup

--- src/app/inventory/maintenance/maintenance.component.ts
+++ src/app/inventory/maintenance/maintenance.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Router} from "@angular/router";
+import {ModalDismissReasons, NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'app-maintenance',
   templateUrl: './maintenance.component.html',
   styleUrls: ['./maintenance.component.scss']
 })
 export class MaintenanceComponent implements OnInit {
+  closeResult: string;
 
-  constructor() { }
+  constructor(private router: Router,
+              private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+  openNewProperty(contentNewProperty) {
+    this.modalService.open(contentNewProperty).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
